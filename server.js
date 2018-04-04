8450af9e1c62200af44616d05014048abcc78bdd
George Treviranus <george@scribd.com>
2018-04-03 21:06:52 -0400

    documents server.js for express

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+// Express server used for Heroku.
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const expressStaticGzip = require('express-static-gzip')
+
+// Uncomment line 10, then comment out line 11 to stop gzip compression.
 
 // app.use(express.static(path.join(__dirname, 'build')));
 app.use("/", expressStaticGzip("build"));
