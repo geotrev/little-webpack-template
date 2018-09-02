eeb2e4ab94e95eda4c419724453c80643fb1ec44
George Treviranus <3946669+geotrev@users.noreply.github.com>
2018-09-02 14:20:39 -0400

    Updates dependencies and environment (#10)
    * update dependencies
    
    * adds resolve instead of join for path
    
    * remove unneeded uglifyjs pl ugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin')
 const common = require('../webpack.common.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+    runtimeChunk: {
+      name: 'manifest'
+    },
+  },
   plugins: [
     // Clean build/ directory before running Webpack
     new CleanWebpackPlugin(
@@ -23,18 +30,7 @@ module.exports = merge(common, {
       algorithm: "gzip",
     }),
   ],
-
   // No need for log vomit
   stats: { children: false },
-
-  // Uglify the output with a source map
-  optimization: {
-    minimizer: [
-      new UglifyJSPlugin({
-        sourceMap: true,
-      }),
-    ],
-  },
-
   mode: 'production'
 });
