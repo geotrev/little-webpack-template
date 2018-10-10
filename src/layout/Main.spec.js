cd6e6765378208da6dba44cc208cd4f616f14920
George Treviranus <hello@geotrev.com>
2018-10-09 20:08:15 -0700

    remove unused routes import in main spec

--- src/layout/Main.spec.js
+++ src/layout/Main.spec.js
@@ -1,6 +1,5 @@
 import Main from "./Main"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Routes from "routes"
 
 describe("<Main />", () => {
   function mountComponent() {
