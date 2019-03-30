80adba340ff6857ccb443b2ae24753dcaeac7f36
george <hello@geotrev.com>
2019-03-30 15:05:54 -0700

    remove mocha, update dependencies, add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    mocha: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+    babelOptions: {
+      configFile: "babel.config.js",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/babel",
+  ],
+  plugins: ["react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+    "no-console": 0,
+  },
+}
--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
-// Install `dynamic-import-node` to devDependencies and add to plugins if you use dynamic imports
-// Use `{ useBuiltIns: "entry" }` on `@babel/preset-env` if you use `@babel/polyfill` at the top src/index.js
 
 module.exports = {
+  // NOTE: Add `{ useBuiltIns: "entry", corejs: 3 }` and install `corejs` and `regenerator-runtime` packages to enable polyfills on `preset-env`
   presets: ["@babel/preset-env", "@babel/preset-react"],
+  // NOTE: Install `dynamic-import-node` to devDependencies and add to plugins to enable d ynamic imports
   plugins: [
     ["babel-plugin-webpack-aliases", { config: "./config/webpack.dev.js" }],
     "@babel/plugin-proposal-class-properties",
