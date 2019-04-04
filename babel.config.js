ee329368dcbcf7e7b5d088fcc93cb33cf0535195
george <hello@geotrev.com>
2019-04-03 22:01:05 -0700

    add browsers list and jest dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,22 @@
+const presets = ["@babel/preset-react", "@babel/preset-env"]
+const plugins = ["@babel/plugin-proposal-class-properties"]
 
-module.exports = {
-  // NOTE: Add `{ useBuiltIns: "entry", corejs: 3 }` and install `corejs` and `regenerator-runtime` packages to enable polyfills on `preset-env`
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  // NOTE: Install `dynamic-import-node` to devDependencies and add to plugins to enable d ynamic imports
-  plugins: [
-    ["babel-plugin-webpack-aliases", { config: "./config/webpack.dev.js" }],
-    "@babel/plugin-proposal-class-properties",
-  ],
+const config = api => {
+  const test = api.env("test")
+
+  if (test) {
+    // remove current instance of @babel/preset-env and add new one with targets defined
+    presets.pop()
+    presets.push(["@babel/preset-env", { targets: { node: "current" } }])
+
+    // to allow webpack aliases to resolve
+    plugins.push(["babel-plugin-webpack-aliases", { config: "./config/webpack.dev.js" }])
+  }
+
+  return {
+    presets,
+    plugins,
+  }
 }
+
+module.exports = config
