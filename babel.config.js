274970d851419269f7bda9c285ca5d6687a35e7f
george <george@scribd.com>
2019-04-04 10:38:11 -0700

    add jest config to package.json

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,7 @@ const presets = ["@babel/preset-react", "@babel/preset-env"]
 const plugins = ["@babel/plugin-proposal-class-properties"]
 
 const config = api => {
-  const test = api.env("test")
-
-  if (test) {
+  if (api.env("test")) {
     // remove current instance of @babel/preset-env and add new one with targets defined
     presets.pop()
     presets.push(["@babel/preset-env", { targets: { node: "current" } }])
--- config/jest.config.js
+++ config/jest.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  clearMocks: true,
-}
+import Enzyme, { shallow, render, mount } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+
+Enzyme.configure({ adapter: new Adapter() })
+
+global.shallow = shallow
+global.render = render
+global.mount = mount
