3bf966cfefef45b19f149324063f0e131a414864
george <hello@geotrev.com>
2019-04-07 14:25:22 -0700

    fix failing config

--- src/pages/About/About.spec.js
+++ src/pages/About/About.spec.js
@@ -1,6 +1,8 @@
 import React from "react"
 import About from "./About"
 
+jest.mock("helpers/ScrollUpOnMount", () => global.simpleMock("ScrollUpOnMount"))
+
 describe("<About />", () => {
   it("matches snapshot", () => {
     const wrapper = mount(<About />)
--- src/pages/Home/Home.spec.js
+++ src/pages/Home/Home.spec.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Home from "./Home"
 
+jest.mock("helpers/ScrollUpOnMount", () => global.simpleMock("ScrollUpOnMount"))
+
 describe("<Home />", () => {
   it("matches snapshot", () => {
     const wrapper = mount(<Home />)
